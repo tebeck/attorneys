6b1252e70a79b96856e494869f4cf4eaf98d7703
Francisco Marasco quiroga <fmarasco@unitedvirtualities.com>
2019-06-18 15:52:57 -0300

    Update API email

--- node-backend/src/services/sendmail.js
+++ node-backend/src/services/sendmail.js
@@ -6,8 +6,8 @@ module.exports = {
 
 	mandrill_client = new mandrill.Mandrill('2-1UFpeIBHy67hD-kxSEZw');
 
-	var raw_message = "From: esquired@flydevs.com\nTo: "+email+"\nSubject: "+subject+"\n\n"+text+"";
-	var from_email = "esquired@flydevs.com";
+	var raw_message = "From: esquired@space-servers.com\nTo: "+email+"\nSubject: "+subject+"\n\n"+text+"";
+	var from_email = "esquired@space-servers.com";
 	var from_name = "Esquired";
 	var to = [
 	    email
@@ -15,7 +15,7 @@ module.exports = {
 	var async = false;
 	var ip_pool = "Main Pool";
 	var send_at = false;
-	var return_path_domain = 'https://flydevs.com';
+	var return_path_domain = 'http://space-servers.com';
 	mandrill_client.messages.sendRaw({"raw_message": raw_message, "from_email": from_email, "from_name": from_name, "to": to, "async": async, "ip_pool": ip_pool, "send_at": send_at, "return_path_domain": return_path_domain}, function(result) {
 	    console.log(result)
 	    
